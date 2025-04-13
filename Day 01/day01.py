1ef16950e2c157edd21f2fdc6bb54d28442e463d
hvmthong <hvmt97@gmail.com>
2025-04-13 16:11:38 +0700

    day01

---
+++ Day 01/day01.py
@@ -0,0 +1,23 @@
+print("Hello, World!")
+name = "Thong"
+age = 28
+print(f"Hello, {name}! You are {age} years old.")
+
+a = 10
+b = 3
+print(f"Sum: {a+b}")
+print(f"Difference: {a-b}")
+print(f"Product: {a*b}")
+print(f"Quotient: {a/b}")
+print(f"Integer Division: {a//b}")
+print(f"Remainder: {a%b}")
+print(f"Exponent: {a**b}")
+print(f"Square root of {a}: {a**0.5}")
+print(f"Square root of {b}: {b**0.5}")
+print(f"Square root of {a} + {b}: {(a+b)**0.5}")
+print(f"Square root of {a} - {b}: {(a-b)**0.5}")
+print(f"Square root of {a} * {b}: {(a*b)**0.5}")
+print(f"Square root of {a} / {b}: {(a/b)**0.5}")
+print(f"Square root of {a} // {b}: {(a//b)**0.5}")
+print(f"Square root of {a} % {b}: {(a%b)**0.5}")
+print(f"Square root of {a} ** {b}: {(a**b)**0.5}")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hello_Python
+This repository is created to practice python!!!
